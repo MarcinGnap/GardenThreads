f9bded2e502cce855ef79ec4bd83be4e7f4b3b74
Marcin Gnap <258953@student.pwr.edu.pl>
2023-03-07 10:52:38 +0100

    1.3 - hello w threads

--- GardenThreads/main.cpp
+++ GardenThreads/main.cpp
@@ -1,10 +1,28 @@
 #include "MainWindow.h"
+#include <thread>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
-	MainWindow window;
+	//	MainWindow window;
 
+	auto hello = [](int x) {
+		cout << "Hello there \nGeneral Kenobi... \nThis is thread: " << this_thread::get_id() << endl;
+		cout << "Kept in: " << x << endl;
+	};
+
+
+	vector<thread> firstThreads;
+	
+	for (int i = 0; i < 10; i++) {
+		firstThreads.push_back(thread(hello, i));
+		firstThreads[i].join();
+	}
+
+
+	cout << "Hello there \nGeneral Kenobi... \nThis is main thread" << endl;
 
 	return 0;
 }
